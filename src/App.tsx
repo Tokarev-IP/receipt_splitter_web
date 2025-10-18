948292b363c3436fab382f2ff2a5e73eb44e8e13
Tokarev-IP <tokarevrab@gmail.com>
2025-10-18 11:11:36 +0100

    improve code

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
 import './App.css';
-import AppAdsTxtPage from './Pages/Info/AppAdsTxtPage';
 import PrivacyPolicyPage from './Pages/Info/PrivacyPolicyPage';
 import DeleteAccountPage from './Pages/Info/DeleteAccountPage';
 import ReceiptPage from './Pages/Receipt/ReceiptPage';
 import ReceiptExamplePage from './Pages/Info/ReceiptExamplePage';
 import ReceiptSplitterPrivacyPolicyPage from './Pages/Info/ReceiptSplitterPrivacyPolicyPage';
+import InfoPage from './Pages/InfoPage';
 
 function Header() {
   return (
@@ -21,18 +21,15 @@ function App() {
   return (
     <div>
       <Router>
-        <Header />
-        <div className="responsive-container">
-          <Routes>
-            <Route path="/" element={<ReceiptPage />} />
-            <Route path="/app-ads.txt" element={<AppAdsTxtPage />} />
-            <Route path="/split-receipt" element={<ReceiptPage />} />
-            <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
-            <Route path="/delete-account" element={<DeleteAccountPage />} />
-            <Route path="/receipt-example" element={<ReceiptExamplePage />} />
-            <Route path="/receipt-splitter-privacy-policy" element={<ReceiptSplitterPrivacyPolicyPage />} />
-          </Routes>
-        </div>
+        <Routes>
+          <Route path="/" element={<><Header /><div className="responsive-container"><InfoPage /></div></>} />
+          <Route path="/info" element={<><Header /><div className="responsive-container"><InfoPage /></div></>} />
+          <Route path="/split-receipt" element={<><Header /><div className="responsive-container"><ReceiptPage /></div></>} />
+          <Route path="/privacy-policy" element={<><Header /><div className="responsive-container"><PrivacyPolicyPage /></div></>} />
+          <Route path="/delete-account" element={<><Header /><div className="responsive-container"><DeleteAccountPage /></div></>} />
+          <Route path="/receipt-example" element={<><Header /><div className="responsive-container"><ReceiptExamplePage /></div></>} />
+          <Route path="/receipt-splitter-privacy-policy" element={<><Header /><div className="responsive-container"><ReceiptSplitterPrivacyPolicyPage /></div></>} />
+        </Routes>
       </Router>
     </div>
   );
